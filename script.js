434e971611f74fa543419acb9112e85bf4c86e34
BahaaKa <>
2021-06-03 12:08:34 +0000

    *03*

---
+++ script.js
@@ -0,0 +1,10 @@
+// 
+var username = 'baha2ka'
+//
+console.log(username);
+//
+alert("WE ARE HERE TO HELP YOU "  +  username  );
+
+document.write("<p style='color:red;' > This article was written by a visitor </p>"); 
+
+prompt('where are you from?') 
