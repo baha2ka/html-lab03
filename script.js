d71d7f4444fcb427b3816755b1c18b5ffda19dc0
BahaaKa <>
2021-06-06 13:31:48 +0000

    *java*

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // 
-var username = 'baha2ka'
-//
+var username = 'baha2ka';
+var myId = 1234;
+var test = true;
+var x = 5;
+var x = 5;
 console.log(username);
-//
-alert("WE ARE HERE TO HELP YOU "  +  username  );
 
-document.write("<p style='color:red;' > This article was written by a visitor </p>"); 
+alert
+("WE ARE HERE TO HELP YOU "  +  username  );
 
-prompt('where are you from?') 
+document.write
+("<p style='color:red;' > This article was written by a visitor </p>"); 
+
+ prompt
+ ('where are you from?');
+
+
+
+ var favfood = prompt("whats your fav food : american ,italian , chinese");
+
+// favfood=italian
+
+function favfood() {
+
+  while (favfood != "american" && favfood != "italian" && favfood != "chinese") {
+
+    favfood = prompt("please enter on of : american, chinese, italian");
+  }
